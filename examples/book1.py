fd1cf4c2e0e53b72ea425c18f2e2f136a3c1c3a6
Oliver Soeser <o.soeser@icloud.com>
2024-06-21 08:49:10 +0100

    Add example

--- examples/book1.py
+++ examples/book1.py
@@ -3,7 +3,7 @@ from manim_euclid import *
 
 class Proposition1(EuclidScene):
     def construct(self) -> None:
-        super().construct("Proposition I.", "On a given finite straight line,\n\n to describe an equilateral triangle.")
+        super().construct("Proposition I.", "To construct an equilateral triangle\n\non a given finite straight line.")
 
         A, B = LEFT, RIGHT
         self.given(Line(A, B))
@@ -15,13 +15,4 @@ class Proposition1(EuclidScene):
         self.postulate_1(A, C, YELLOW)
         self.postulate_1(B, C, RED)
 
-        self.wait()
-
-class Proposition2(EuclidScene):
-    def construct(self) -> None:
-        super().construct("Proposition II.", "From a given point, to draw a straight line\n\n equal to a given finite straight line.")
-
-        A, B, P = [-2, 0, 0], [0, -1, 0], [1, 0.5, 0]
-
-        self.given(Line(A, B), Dot(P))
         self.wait()
